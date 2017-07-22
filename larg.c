96c23eec4ffd3c7f4cfada3c8e41fe969455638f
AarthiMonicka <aarthimonickaam@gmail.com>
2017-07-22 17:52:15 +0530

    Update larg.c

--- larg.c
+++ larg.c
@@ -5,10 +5,10 @@ int a,b,c;
 printf("enter values:");
 scanf("%d %d %d",&a,&b,&c);
 if(a>b&&a>c)
-printf("%d is larger",a);
+printf("a is larger",a);
 elseif(b>a&&b>c)
-printf("%d is larger",b);
+printf("b is larger",b);
 else
-printf("%d is larger",c);
+printf("c is larger",c);
 return 0;
 }
