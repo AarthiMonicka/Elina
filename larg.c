5ad6a0df75600a4332c8e475f90c6b028b2c8c96
AarthiMonicka <aarthimonickaam@gmail.com>
2017-07-22 17:55:44 +0530

    Update larg.c

--- larg.c
+++ larg.c
@@ -4,9 +4,9 @@ int main()
 int a,b,c;
 printf("enter values:");
 scanf("%d %d %d",&a,&b,&c);
-if(a>b&&a>c)
+if(a>b && a>c)
 printf("a is larger",a);
-elseif(b>a&&b>c)
+elseif(b>a && b>c)
 printf("b is larger",b);
 else
 printf("c is larger",c);
