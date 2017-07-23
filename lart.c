cd45573f7b04d42799cb00769cf79e14fa6b62b7
AarthiMonicka <aarthimonickaam@gmail.com>
2017-07-23 14:46:32 +0530

    Update and rename largest.c to lart.c

--- lart.c
+++ lart.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
-int main()
+void main()
 {
 int a,b,c;
 printf("enter values:");
 scanf("%d%d%d",&a,&b,&c);
 if(a>b && a>c)
+{
 printf("a is larger",a);
+}
 elseif(b>a && b>c)
-printf("b is larger",b);
-else
-printf("c is larger",c);
-return 0;
+{
+  printf("b is larger",b);
+}
+ else
+ {
+ printf("c is larger",c);
+ }
 }
