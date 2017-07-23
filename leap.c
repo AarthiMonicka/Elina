03083ec9a51f63569026e3780aab0663edb3dfb2
AarthiMonicka <aarthimonickaam@gmail.com>
2017-07-23 10:30:36 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -5,9 +5,9 @@ int n;
 printf("Enter the year");
 scanf("%d",&n);
 if((n%400)==0)
-ptintf("leap year");
+printf("leap year");
 elseif((n%100)==0)
-ptintf("not a leap year");
+printf("not a leap year");
 elseif((n%4)==0)
 printf("leap year");
 else
