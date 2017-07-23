83bbac6a7556e4baf0075f46b4e884f2e41402de
AarthiMonicka <aarthimonickaam@gmail.com>
2017-07-23 14:49:25 +0530

    Update lart.c

--- lart.c
+++ lart.c
@@ -5,11 +5,11 @@ void main()
 int a,b,c;
 printf("enter values:");
 scanf("%d%d%d",&a,&b,&c);
-if(a>b && a>c)
+if(a>b&&a>c)
 {
 printf("a is larger",a);
 }
-elseif(b>a && b>c)
+elseif(b>a&&b>c)
 {
   printf("b is larger",b);
 }
