df99f9a7d5704f510f8daf1bb17e4cb1ba40380e
AarthiMonicka <aarthimonickaam@gmail.com>
2017-07-24 10:32:27 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
-int val,exp,c,res=0;
+int val,exp,c,res=0,s;
 printf("Enter the values:");
-scanf("%d\t%d",val,exp);
-for(c=0;c<exp;c++)
+scanf("%d%d",&val,&exp);
+for(c=0;c<=exp;c++)
 {
 res=res*val;
 }
-printf("the result is:%d",res);
+printf("\nthe result is:%d",res);
+getch();
 return 0;
 }
